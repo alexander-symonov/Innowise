377e19c3893b7607c72581288a814544f27a87cf
AlexanderS-net <develop.alexander.symonov@gmail.com>
2025-03-31 15:40:46 +0200

    Add DataGenerator project. Add DTO common project. Add common proto models

---
+++ src/DataGenerator/EventGenerator.cs
@@ -0,0 +1,86 @@
+﻿using Confluent.Kafka;
+using DTO.InsuranceIncidents.Car;
+using Google.Protobuf;
+
+namespace DataGenerator
+{
+    public class EventGenerator
+    {
+        ProducerConfig _producerConfig;
+        public EventGenerator(string brokerList)
+        {
+            _producerConfig = new ProducerConfig { BootstrapServers = brokerList };
+        }
+        public async Task Generate(string type, int count, int delayMs)
+        {
+            using var producer = new ProducerBuilder<string, byte[]>(_producerConfig).Build();
+            var topicName = "insurance-incidents-" + type;
+
+            for (int i = 0; i < count; i++)
+            {
+                var message = GenerateEvent(type, i);
+                SendMessage(Serialize(message), producer, topicName);
+
+                await Task.Delay(delayMs);
+            }
+        }
+
+        private IMessage GenerateEvent(string type, int number)
+        {
+            return true switch
+            {
+                true when typeof(CarIncident).Name == type => GetCarIncident(),
+                true when typeof(FlatIncident).Name == type => GetFlatIncident(),
+                _ => throw new ArgumentException("Unsupported model " + type)
+            };
+        }
+
+        public byte[] Serialize(IMessage record)
+        {
+            using (var stream = new MemoryStream())
+            {
+                using (var codedStream = new CodedOutputStream(stream))
+                {
+                    record.WriteTo(codedStream);
+                    codedStream.Flush();
+                    return stream.ToArray();
+                }
+            }
+        }
+
+        private CarIncident GetCarIncident()
+        {
+            return new CarIncident()
+            {
+                VIN = Guid.NewGuid().ToString(),
+                Model = "Model" + DateTime.UtcNow.Second,
+                OwnerNumber = Random.Shared.Next(100).ToString()
+            };
+        }
+
+        private FlatIncident GetFlatIncident()
+        {
+            return new FlatIncident()
+            {
+                OwnerNumber = Random.Shared.Next(100).ToString()
+            };
+        }
+
+        private void SendMessage(byte[] eventData, IProducer<string, byte[]> producer, string topic)
+        {
+            try
+            {
+                var deliveryResult = producer.ProduceAsync(
+                    topic, 
+                    new Message<string, byte[]> { Key = Guid.NewGuid().ToString(), Value = eventData })
+                    .GetAwaiter().GetResult();
+                
+                Console.WriteLine($"Message delivered to {deliveryResult.TopicPartitionOffset}");
+            }
+            catch (ProduceException<string, byte[]> e)
+            {
+                Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+            }
+        }
+    }
+}
---
+++ src/DataGenerator/Program.cs
@@ -0,0 +1,36 @@
+﻿using DataGenerator;
+
+var deleayMs = 100;
+var brokersList = Environment.GetEnvironmentVariable("BROKERS_LIST") ?? "localhost:9095";
+var eventGenerator = new EventGenerator(brokersList);
+if(args.Length >= 2)
+{
+    Run(args[0], int.Parse(args[1]));
+}
+
+do
+{
+    Console.WriteLine("Enter command. Example: CarIncident 100");
+    var command = Console.ReadLine().Split(" ");
+    if (command.Length == 2) {
+        Run(command[0], int.Parse(command[1])); 
+    }
+    else { 
+        Console.WriteLine("Invalid command"); 
+    }
+
+} while (true);
+
+
+
+async Task Run(string type, int count)
+{
+    try
+    {
+        await eventGenerator.Generate(type, count, deleayMs);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+}
